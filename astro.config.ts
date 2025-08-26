4f219a570be5565cb8d3c4b856c3ffdd603d1f7e
Franklin Sula <f.sula@a2kgroup.org>
2025-08-26 15:38:04 +0800

    feature: added login feature

--- astro.config.ts
+++ astro.config.ts
@@ -13,6 +13,7 @@ import astroIcon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   site: 'https://example.com',
   integrations: [
     mdx(),
